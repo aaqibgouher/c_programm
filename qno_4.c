ab70a450341e1d766b6a571474b08a63d0d81147
aaqibgouher <aaqibgouher@gmail.com>
2019-05-16 23:50:44 +0530

    added a new program

---
+++ qno_4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    char name[100] = "Aaqib";
+    int n = strlen(name);
+    int i,k;
+    for(i=0;i<n/2;i++){
+        k=name[i];
+        name[i] = name[n-1-i];
+        name[n-1-i] = k;
+    }
+    printf("%s ",name);
+}

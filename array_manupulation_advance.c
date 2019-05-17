5968c3e6e2a6e1be94b84129b6b8ff18328fa836
Nazish Fraz <nfraz007@gmail.com>
2019-05-18 01:06:01 +0530

    added question

--- array_manupulation_advance.c
+++ array_manupulation_advance.c
@@ -27,6 +27,21 @@ void remove(int position){
 
 }
 
+// it will show the array in revere order
+void reverse(){
+
+}
+
+// it will show the index of the element if exist in array, else show not found
+void find(int k){
+
+}
+
+// it will show the sub array - both start n end are inclusive
+void sub_array(int start, int end){
+    
+}
+
 void main()
 {
     display();
@@ -34,7 +49,11 @@ void main()
     push(5); // 2 5 // 2 6 5
     insert(6,2); // 2 6 5
     display();
-    remove(2); // 2 5
-    pop(); // 2
+    reverse(); // 5 6 2
+    find(6); // index - 2
+    push(3); // 2 6 5 3
+    sub_array(1,2); // 6 5
+    remove(2); // 2 5 3
+    pop(); // 2 5
     display();
 }

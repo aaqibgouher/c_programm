2c558d694fcb20100b8a1d4d6428fb9be37a8d1d
Nazish Fraz <nfraz007@gmail.com>
2019-05-18 01:00:59 +0530

    added question

---
+++ array_manupulation_advance.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int a[10000],n=0;
+
+// this function will display arr seperated by white space, nd finally new line
+void display(){
+
+}
+
+// it will insert k in the last of the array
+void push(int k){
+    
+}
+
+// it will insert at specific position, 
+void insert(int k, int position){
+    
+}
+
+// it will remove last element
+void pop(){
+    
+}
+
+// it will remove from that position
+void remove(int position){
+
+}
+
+void main()
+{
+    display();
+    push(2); // 2
+    push(5); // 2 5 // 2 6 5
+    insert(6,2); // 2 6 5
+    display();
+    remove(2); // 2 5
+    pop(); // 2
+    display();
+}
